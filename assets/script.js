dbe5be7e40abee9480f3d1f16165846cc715d71d
leonfreiberg <56138678+leonfreiberg@users.noreply.github.com>
2022-07-02 09:33:50 +0200

    Test ar modes

--- assets/script.js
+++
@@ -1,4 +0,0 @@
-customElements.whenDefined( 'model-viewer' ).then( () => {  
-    var ModelViewer= document.querySelector('model-viewer').shadowRoot;
-    ModelViewer.querySelector('#view_x5F_in_x5F_AR_x5F_icon').style.display = "none";
-} )
